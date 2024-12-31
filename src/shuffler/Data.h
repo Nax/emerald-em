9abbde0b17b548cfba6b8dc47c47fba5ba09822a
Nax <4265415+Nax@users.noreply.github.com>
2024-12-31 03:23:46 +0100

    Builds and execs

--- src/shuffler/Data.h
+++ src/shuffler/Data.h
@@ -1,9 +1,9 @@
 #ifndef DATA_H
 #define DATA_H
 
-#define DATA_SPECIES_SIZE                       0xc4
+#define DATA_SPECIES_SIZE                       0xf4
 #define DATA_SPECIES_OFF_STATS                  0x00
 #define DATA_SPECIES_OFF_ABILITIES              0x18
-#define DATA_SPECIES_OFF_LEVELUP_LEARNSET       0x80
+#define DATA_SPECIES_OFF_LEVELUP_LEARNSET       0x84
 
 #endif
